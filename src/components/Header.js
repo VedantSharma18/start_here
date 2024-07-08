2462a6f49787f60ca02eb25e451f5088673c1a83
VedantSharma18 <vedantsharma18@gmail.com>
2024-07-08 20:13:34 +0530

    Start Here page done , only color palette left

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <h1>Welcome to Yummy Yard</h1>
+        
+        <pre>
+            Feast your eyes on our mouth watering recipes <br />
+            <br />Sometimes you just need a steamy bowl of Biryani to soothe your taste buds!
+            <br />or , maybe a portion of Fish Florentine to cream away your sorrows.
+            <br />  We have got it all here with much more!!
+            <br />Share your recipes which tantalize the senses and display the divinity of food!
+        </pre>
+
+        </div>
+        
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/MainContent.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './MainContent.css';
+
+const MainContent = () => {
+  return (
+    <main className="main-content">
+      <section className="section">
+        <h2>Login/Signup</h2>
+        <p> Post your lip smacking recipes</p>
+        <h2>About Us</h2>
+        <p>Read our </p>
+      </section>
+      <section className="section">
+        <h2>Recipes</h2>
+        <p>See our magical recipes</p>
+      </section>
+    </main>
+  );
+};
+
+export default MainContent;
